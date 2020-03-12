e83e767f05f98a7882476f386bb627ca01398217
Marc Poulhiès <dkm@kataplop.net>
2020-03-12 20:54:10 +0100

    disable some option to avoid potential problem
    Disable hw-loops and load/store packing as they could explain the currents errors
    in building libcore.

--- src/librustc_codegen_llvm/llvm_util.rs
+++ src/librustc_codegen_llvm/llvm_util.rs
@@ -104,6 +104,8 @@ unsafe fn configure_llvm(sess: &Session) {
         // HACK(eddyb) LLVM inserts `llvm.assume` calls to preserve align attributes
         // during inlining. Unfortunately these may block other optimizations.
         add("-preserve-alignment-assumptions-during-inlining=false", false);
+        add("-disable-kvx-hwloops", false);
+        add("-disable-kvx-loadstore-packing", false);
 
         for arg in sess_args {
             add(&(*arg), true);
